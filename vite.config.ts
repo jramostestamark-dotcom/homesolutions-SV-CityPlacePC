1ec50527601336c19677a589d0da771ea1961747
jramostestamark-dotcom <jramostestamark@gmail.com>
2026-05-20 12:35:43 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
+ 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html'
+    }
+  }
 })
